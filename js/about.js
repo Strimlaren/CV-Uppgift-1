f44ab8f5b5cb021b97724d42e21cd78ed400f961
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2023-11-22 23:42:06 +0100

    Refactored some code in about.js. Variable names for less confusion, and added comments

--- js/about.js
+++ js/about.js
@@ -7,9 +7,9 @@ async function getCV() {
     const data = await cv_data.json();
     // A for loop for each section of the CV. Will iterate depending on amount of items inside each object. Will dynamically create the content and insert to hard-coded HTML wrappers depending on how many items there are in the JSON. If the amount of items changes in the JSON in the future, function should correctly extend the webpage with this new content with no need for changing any HTML.
     // General Skills
-    const skills_progress = document.getElementById("skills-progress");
+    const my_skills = document.getElementById("skills-progress");
     for (const item in data.skills_progress) {
-      // Creating one element at a time and giving it classes, and content from the JSON. For readability, appends last
+      // Creating one element at a time and giving it classes, and content from the JSON. For readability. Appends last
       const p_tag = document.createElement("p");
       p_tag.textContent = capitalize(item);
 
@@ -18,15 +18,15 @@ async function getCV() {
 
       const progress_div = document.createElement("div");
       progress_div.classList.add("progress");
-      progress_div.classList.add(`${data.skills_progress[item]}`); // DO I NEED THIS ???
+      progress_div.classList.add(`${data.skills_progress[item]}`);
 
       container_div.appendChild(progress_div);
-      skills_progress.appendChild(p_tag);
-      skills_progress.appendChild(container_div);
+      my_skills.appendChild(p_tag);
+      my_skills.appendChild(container_div);
     }
 
     // Programming skills
-    const programming_progress = document.getElementById("programming-progress");
+    const my_programming = document.getElementById("programming-progress");
     for (const item in data.programming_progress) {
       const p_tag = document.createElement("p");
       p_tag.textContent = item.toUpperCase();
@@ -39,12 +39,12 @@ async function getCV() {
       progress_div.classList.add(`${data.programming_progress[item]}`);
 
       container_div.appendChild(progress_div);
-      programming_progress.appendChild(p_tag);
-      programming_progress.appendChild(container_div);
+      my_programming.appendChild(p_tag);
+      my_programming.appendChild(container_div);
     }
 
     // Language Skills
-    const languages_progress = document.getElementById("languages-progress");
+    const my_languages = document.getElementById("languages-progress");
     for (const item in data.languages_progress) {
       const p_tag = document.createElement("p");
       p_tag.textContent = capitalize(item);
@@ -57,22 +57,22 @@ async function getCV() {
       progress_div.classList.add(`${data.languages_progress[item]}`);
 
       container_div.appendChild(progress_div);
-      languages_progress.appendChild(p_tag);
-      languages_progress.appendChild(container_div);
+      my_languages.appendChild(p_tag);
+      my_languages.appendChild(container_div);
     }
 
     // Hobbies
-    const hobbies = document.getElementById("hobbies");
+    const my_hobbies = document.getElementById("hobbies");
     data.hobbies.forEach(function (hobby) {
-      hobbies.innerHTML += `${capitalize(hobby)}<br>`;
+      my_hobbies.innerHTML += `${capitalize(hobby)}<br>`;
     });
 
     // Profile
-    const profile = document.getElementById("profile-text");
-    profile.textContent = data.profile;
+    const my_profile = document.getElementById("profile-text");
+    my_profile.textContent = data.profile;
 
     // Work Experience
-    const work_experiences = document.getElementById("work-experience");
+    const my_work_experiences = document.getElementById("work-experience");
     iteration_number = 0;
     for (const item in data.work_experience) {
       const h5_tag = document.createElement("h5");
@@ -87,17 +87,17 @@ async function getCV() {
 
       const hr_item = document.createElement("hr");
 
-      work_experiences.appendChild(h5_tag);
-      work_experiences.appendChild(dates_p_tag);
-      work_experiences.appendChild(description_p);
+      my_work_experiences.appendChild(h5_tag);
+      my_work_experiences.appendChild(dates_p_tag);
+      my_work_experiences.appendChild(description_p);
       // I dont want a <hr> after last item
       if (iteration_number != data.work_experience.length - 1)
-        work_experiences.appendChild(hr_item);
+        my_work_experiences.appendChild(hr_item);
       iteration_number++;
     }
 
     // Education
-    const education = document.getElementById("education");
+    const my_education = document.getElementById("education");
     iteration_number = 0;
     for (const item in data.education) {
       const h5_tag = document.createElement("h5");
@@ -110,15 +110,14 @@ async function getCV() {
       const description_p = document.createElement("p");
       description_p.textContent = data.education[item].description;
 
-      education.appendChild(h5_tag);
-      education.appendChild(dates_p_tag);
-      education.appendChild(description_p);
+      my_education.appendChild(h5_tag);
+      my_education.appendChild(dates_p_tag);
+      my_education.appendChild(description_p);
     }
   }
+  // After everything is loaded in, animate the progress bars
   change_progress();
 }
-
-getCV();
 
 // Take a string and return it with first char of every word capitalized
 function capitalize(string) {
@@ -129,22 +128,27 @@ function capitalize(string) {
   }
   return words.join(" ");
 }
+
 // AUTHOR: EXTERNAL
 // LINK: https://codepen.io/theprogrammingexpert/pen/jOqGBLL
 function change_progress() {
-
+  // Get all bars with class progress
   const progress_bars = document.querySelectorAll(".progress");
-
+  // For each bar found, run a personalized animation
   progress_bars.forEach(function (bar) {
+    // Calculate how much 1% of this bars progress is. Fetch value from classlist
     const hundredth = Number(bar.classList[1]) / 100;
-
+    // Run animation in 100 steps for smoothness
     for (let i = 1; i < 101; i++) {
+      // Animate the bars in 100 increments and with 40ms delay between each
       setTimeout(() => change_progress(hundredth * i, bar), 40 * i);
     }
-
+    // Arrowfunction to change the div width and % text on the bar. CSS handles transition
     const change_progress = (progress, my_bar) => {
       my_bar.style.width = `${progress}%`;
       my_bar.textContent = `${Math.round(progress)}%`;
     }
   })
 }
+
+getCV();
