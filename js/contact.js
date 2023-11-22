43dd07afc3b2710ef69fbfc96646f2da37d2d0bc
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2023-11-22 12:43:21 +0100

    reworked entire image library with smaller sized versions. Refactored contact.js script and added comments.

--- js/contact.js
+++ js/contact.js
@@ -1,35 +1,25 @@
-let name_input = document.getElementById("name");
-let email_input = document.getElementById("email");
-let name_char_display = document.getElementById("name-counter");
-let email_char_display = document.getElementById("email-counter");
+// AUTHOR: Mirza Mesinovic
+// WHAT IS IT?: Counter that keeps track of and displays amount of allowed chars left to type in a text-field
 
-name_input.addEventListener("input", update_name_counter);
-email_input.addEventListener("input", update_email_counter);
+const message_input = document.getElementById("message");
+const chars_left = document.getElementById("chars-left");
+const max_chars = document.getElementById("max-chars"); 
+// Litsen for whenever user inputs another char into the textarea field
+message_input.addEventListener("input", update_message_counter);
+// Get maxlength attribute from textarea and use it to set starting values that are presented to the user in the format (CHARS LEFT / MAX ALLOWED CHARS)
+chars_left.textContent = message_input.attributes.maxlength.value;
+max_chars.textContent = message_input.attributes.maxlength.value;
 
-function update_name_counter() {
-
-  let chars = 30 - (name_input.value).length;
-  name_char_display.textContent = chars;
-
-  if (chars < 11) {
-    name_char_display.classList.add("warning");
-    name_input.classList.add("warning-input");
+function update_message_counter() {
+  // Calculate how many more chars user is allowed to type
+  const chars = Number(max_chars.textContent) - (message_input.value).length;
+  chars_left.textContent = chars;
+  // Visual queue the user with css when less than 10% of max allowed chars remaining
+  if (chars < Number(max_chars.textContent) / 10) {
+    chars_left.classList.add("warning");
+    message_input.classList.add("warning-input");
   } else {
-    name_char_display.classList.remove("warning");
-    name_input.classList.remove("warning-input");
-  }
-}
-
-function update_email_counter() {
-  
-  let chars = 30 - (email_input.value).length;
-  email_char_display.textContent = chars;
-
-  if (chars < 11) {
-    email_char_display.classList.add("warning");
-    email_input.classList.add("warning-input");
-  } else {
-    email_char_display.classList.remove("warning");
-    email_input.classList.remove("warning-input");
+    chars_left.classList.remove("warning");
+    message_input.classList.remove("warning-input");
   }
 }
