b39715f3870e552fd0d6748a0c61c06e104c3103
Mirza Mesinovic <mirza.mesinovic@chasacademy.com>
2024-01-28 10:38:07 +0100

    added gtag to all pages

--- games/rps/main.js
+++ games/rps/main.js
@@ -7,29 +7,62 @@ choice_button.addEventListener("click", cycle_signs);
 reset_button.addEventListener("click", reset);
 
 const announcer = document.getElementById("announcer");
-const excuses = ["I was tired!", "I looked away!", "Luck!", "Who are you?!", "I was changing tires!", "It's cold over here!", "I wasn't trying!", "I'm just warming up..", "Did we start already?", "Neo, is that you?", "I was doing my nails!", "I wasn't ready!", "My water just broke..", "I dropped my taco..", "What are the rules again?", "Im not a real CPU..", "I wasn't given an AI..", "You're just guessing!", "I'm depressed. Mortgage.. kids.. WIFE!"];
-const mocking = ["You need a tissue?", "You are bad at this..", "Damn, I'm good.", "Why are you still here?", "I can do this all day!", "I pity the fool!", "PAIN!", "Cry me a river!", "Aww, you gonna cry?", "I've been training for years!", "I never lose!", "Are you trolling?", "I'm so good I'm scary.", "Hah! I'm reading your mind!", "Yo momma's not here!"]
+const excuses = [
+  "I was tired!",
+  "I looked away!",
+  "Luck!",
+  "Who are you?!",
+  "I was changing tires!",
+  "It's cold over here!",
+  "I wasn't trying!",
+  "I'm just warming up..",
+  "Did we start already?",
+  "Neo, is that you?",
+  "I was doing my nails!",
+  "I wasn't ready!",
+  "My water just broke..",
+  "I dropped my taco..",
+  "What are the rules again?",
+  "Im not a real CPU..",
+  "I wasn't given an AI..",
+  "You're just guessing!",
+  "I'm depressed. Mortgage.. kids.. WIFE!",
+];
+const mocking = [
+  "You need a tissue?",
+  "You are bad at this..",
+  "Damn, I'm good.",
+  "Why are you still here?",
+  "I can do this all day!",
+  "I pity the fool!",
+  "PAIN!",
+  "Cry me a river!",
+  "Aww, you gonna cry?",
+  "I've been training for years!",
+  "I never lose!",
+  "Are you trolling?",
+  "I'm so good I'm scary.",
+  "Hah! I'm reading your mind!",
+  "Yo momma's not here!",
+];
 
 function cycle_signs() {
   // Get list of classes of the button to check which sign is currently selected
   const sign = choice_button.classList;
   // Check which sign is currently selected and cycle to the next on click
-  
+
   if (sign[1] === "rock" || sign[2] === "rock") {
     sign.remove("rock");
     sign.add("scissors");
-    
-    graphic.src = "images/scissors.svg"
-  }
-  else if (sign[1] === "scissors" || sign[2] === "scissors") {
+    graphic.src = "images/scissors.svg";
+  } else if (sign[1] === "scissors" || sign[2] === "scissors") {
     sign.remove("scissors");
     sign.add("paper");
-    graphic.src = "images/paper.svg"
-  }
-  else if (sign[1] === "paper" || sign[2] === "paper") {
+    graphic.src = "images/paper.svg";
+  } else if (sign[1] === "paper" || sign[2] === "paper") {
     sign.remove("paper");
     sign.add("rock");
-    graphic.src = "images/rock.svg"
+    graphic.src = "images/rock.svg";
   }
 }
 
@@ -38,17 +71,16 @@ const play_button = document.getElementById("play");
 play_button.addEventListener("click", play_round);
 
 function play_round() {
-
   play_button.disabled = true;
   cpu_choice.classList.remove("winner");
   choice_button.classList.remove("winner");
 
   let cpu_pick = Math.floor(Math.random() * 3);
-  
+
   if (cpu_pick == 0) c_graphic.src = "images/rock.svg";
   if (cpu_pick == 1) c_graphic.src = "images/scissors.svg";
   if (cpu_pick == 2) c_graphic.src = "images/paper.svg";
-  
+
   const sign = choice_button.classList;
   const p_score = document.getElementById("p-score");
   const c_score = document.getElementById("cpu-score");
@@ -75,7 +107,7 @@ function play_round() {
   function cpu_win() {
     c_score.textContent = Number(c_score.textContent) + 1;
     cpu_choice.classList.add("winner");
-    announcer.textContent = "ROUND TO CPU >"
+    announcer.textContent = "ROUND TO CPU >";
     c_info.textContent = mocking[Math.floor(Math.random() * mocking.length)];
     history.innerHTML += `<span class="loss">L </span>`;
   }
@@ -91,7 +123,7 @@ function play_round() {
   function draw() {
     cpu_choice.classList.remove("winner");
     choice_button.classList.remove("winner");
-    announcer.textContent = "< Draw! >"
+    announcer.textContent = "< Draw! >";
     c_info.textContent = "Boring!";
     history.innerHTML += `<span class="draw">D </span>`;
   }
@@ -100,7 +132,7 @@ function play_round() {
 
   if (Number(p_score.textContent) == 5) {
     play_button.disabled = true;
-    announcer.textContent = "YOU WON!"
+    announcer.textContent = "YOU WON!";
     c_info.textContent = "I don't like this game :(";
     play_button.textContent = "GAME OVER";
     return;
@@ -108,7 +140,7 @@ function play_round() {
 
   if (Number(c_score.textContent) == 5) {
     play_button.disabled = true;
-    announcer.textContent = "YOU LOST!"
+    announcer.textContent = "YOU LOST!";
     c_info.textContent = "Get rekt noob!";
     play_button.textContent = "GAME OVER";
     return;
